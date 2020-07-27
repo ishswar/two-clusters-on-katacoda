40b06757a5ccc01019ee5346296e157610371dbb
Pranay Shah <pshah@tibco.com>
2020-07-27 14:04:42 -0700

    Update to script - update banner

--- script/two_clusters.sh
+++ script/two_clusters.sh
@@ -9,6 +9,15 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+banner()
+{
+  echo "+------------------------------------------+"
+  printf "| %-40s |\n" "`date`"
+  echo "|                                          |"
+  printf "|`tput bold` %-40s `tput sgr0`|\n" "$@"
+  echo "+------------------------------------------+"
+}
+
 HOST_NAME=$(hostname)
 
 MACHINE_ONE=controlplane
@@ -16,9 +25,9 @@ MACHINE_TWO=node01
 
 if [ "$HOST_NAME" = "$MACHINE_ONE" ]; then
         {
-                echo "Installing K3S Cluster on Control Plan machine"
+                banner "Installing K3S Cluster on Control Plan machine"
                 curl -sfL https://get.k3s.io | sh -
-                echo "Now will try to get second cluster kube config and merge it with local config"
+                banner "Now will try to get second cluster kube config and merge it with local config"
                 mkdir -p .kube
                 n=0
 				while [ "$n" -lt 10 ] && ! scp $MACHINE_TWO:/etc/rancher/k3s/k3s.yaml $MACHINE_TWO.config.yaml; do
@@ -29,10 +38,15 @@ if [ "$HOST_NAME" = "$MACHINE_ONE" ]; then
 				done
 				FILE=$MACHINE_TWO.config.yaml
 				if [ -f "$FILE" ]; then
+					banner "Downloaded Kube config from node01 - now merging it locally"
 					sed -i 's/default/cluster2/g' $MACHINE_TWO.config.yaml
 					sed -i "s/127.0.0.1/$MACHINE_TWO/g" $MACHINE_TWO.config.yaml
+
+					sed -i 's/default/cluster1/g' /etc/rancher/k3s/k3s.yaml
+
 					export KUBECONFIG=/etc/rancher/k3s/k3s.yaml:$MACHINE_TWO.config.yaml
 					kubectl config view --flatten > ~/.kube/config
+					banner "Swithing kubectl context to cluster2"
 					kubectl config use-context cluster2
 					kubectl config get-contexts
 					kubectl get nodes
