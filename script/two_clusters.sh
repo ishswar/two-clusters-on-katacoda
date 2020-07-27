9518ace3f42b72360fa85c5b6a29ce51705fe8bb
Pranay Shah <pshah@tibco.com>
2020-07-27 13:50:48 -0700

    Update to script

--- script/two_clusters.sh
+++ script/two_clusters.sh
@@ -5,6 +5,10 @@
 ### Helper script ####
 ######################
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
 HOST_NAME=$(hostname)
 
 MACHINE_ONE=controlplane
@@ -23,19 +27,30 @@ if [ "$HOST_NAME" = "$MACHINE_ONE" ]; then
 				    echo "Looks like $MACHINE_TWO config is not ready yet will try $TURNLEFT more time"
 				    sleep 10
 				done
-				sed -i 's/default/cluster2/g' $MACHINE_TWO.config.yaml
-				sed -i "s/127.0.0.1/$MACHINE_TWO/g" $MACHINE_TWO.config.yaml
-				export KUBECONFIG=/etc/rancher/k3s/k3s.yaml:$MACHINE_TWO.config.yaml
-				kubectl config view --flatten > ~/.kube/config
-				kubectl config use-context cluster2
-				kubectl config get-contexts
-				kubectl get nodes
+				FILE=$MACHINE_TWO.config.yaml
+				if [ -f "$FILE" ]; then
+					sed -i 's/default/cluster2/g' $MACHINE_TWO.config.yaml
+					sed -i "s/127.0.0.1/$MACHINE_TWO/g" $MACHINE_TWO.config.yaml
+					export KUBECONFIG=/etc/rancher/k3s/k3s.yaml:$MACHINE_TWO.config.yaml
+					kubectl config view --flatten > ~/.kube/config
+					kubectl config use-context cluster2
+					kubectl config get-contexts
+					kubectl get nodes
+				else 
+				    echo "$FILE does not exist."
+				    exit
+				fi
+
         }
 elif [ "$HOST_NAME" = "$MACHINE_TWO" ]; then
         {
-                echo "no"
+                echo "Processing node01 installing k3s cluster"
                 curl -sfL https://get.k3s.io | sh -
         }
+else
+	{
+				echo "No Host matched for processing"
+	}
 fi
 
 
