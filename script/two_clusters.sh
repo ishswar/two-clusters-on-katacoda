84f88492236692c33b7f4d73682aa8f1e0ee136f
Pranay Shah <pshah@tibco.com>
2020-07-27 17:41:02 -0700

    Update to script - added more comments and echo commands

--- script/two_clusters.sh
+++ script/two_clusters.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 ######################
-### Helper script - Creating cluster ####
+### Helper script - TWO K3S Clusters on two katakoda ubutnu machines - for kubectl "set-context" practice ####
 ######################
 
 set -o errexit
@@ -25,61 +25,67 @@ MACHINE_TWO=node01
 
 if [ "$HOST_NAME" = "$MACHINE_ONE" ]; then
         {
-                banner "Installing K3S Cluster on Control Plan machine"
-                echo "Running command: [curl -sfL https://get.k3s.io | sh -]"
+                banner "Installing K3S Cluster on [$MACHINE_ONE]] machine"
+                echo "Running command: [curl -sfL https://get.k3s.io | sh -] to install K3S locally on machine $MACHINE_ONE"
                 curl -sfL https://get.k3s.io | sh -
-                echo "Running command: [kubectl config get-contexts] to check kubec context avalable"
-                kubectl config get-contexts 
-                echo "Running command: [kubectl get nodes] to check if install is successfull or not"
-                kubectl get nodes || { echo "K3S Ssetup seems to have issue needs to be invastigated" && exit; }
-                banner "Now will try to get second cluster kube config and merge it with local config"
-                mkdir -p .kube
+                echo "Running command: [kubectl config get-contexts] to check kubectl context avalable"
+                #Eventually we will move /etc/rancher/k3s/k3s.yaml to ~/.kube/config file for ease of use
+                kubectl --kubeconfig=/etc/rancher/k3s/k3s.yaml config get-contexts 
+                echo "Running command: [kubectl --kubeconfig=/etc/rancher/k3s/k3s.yaml get nodes] to check if install is successfull or not"
+                kubectl --kubeconfig=/etc/rancher/k3s/k3s.yaml get nodes || { echo "*********** K3S Ssetup seems to have issue needs to be invastigated ************" && exit; }
+                banner "Now will try to get kube config from second cluster (on machine $MACHINE_TWO) and merge it with local config"
                 echo "Running command: [scp -q -o LogLevel=QUIET $MACHINE_TWO:/etc/rancher/k3s/k3s.yaml $MACHINE_TWO.config.yaml] to get remote cluster config file"
+                # We assume remote machine has already k3s cluster installed and SSH is possible without credentails 
                 n=0
 				while [ "$n" -lt 10 ] && ! scp -q -o LogLevel=QUIET $MACHINE_TWO:/etc/rancher/k3s/k3s.yaml $MACHINE_TWO.config.yaml; do
 				    n=$(( n + 1 ))
 				    TURNLEFT=$(( 10 - $n ))
-				    echo "Looks like $MACHINE_TWO config is not ready yet will try $TURNLEFT more time"
+				    echo "Looks like $MACHINE_TWO K3S config is not ready yet; will try $TURNLEFT more time (after 10 seconds)"
 				    sleep 10
 				done
 				FILE=$MACHINE_TWO.config.yaml
 				if [ -f "$FILE" ]; then
-					banner "Done downloaded Kube config from $MACHINE_TWO - now merging it locally"
+					echo "Done downloaded Kube config from $MACHINE_TWO - now merging it locally"
 
-					sed -i 's/default/cluster2/g' $MACHINE_TWO.config.yaml
-					sed -i "s/127.0.0.1/$MACHINE_TWO/g" $MACHINE_TWO.config.yaml
+					echo "Updating [$FILE] for cluster info (name,IP etc)"
+					sed -i 's/default/cluster2/g' $FILE
+					sed -i "s/127.0.0.1/$MACHINE_TWO/g" $FILE
 
+					echo "Updating file [/etc/rancher/k3s/k3s.yaml] local cluster info for cluster context name - change it from 'default' to 'cluster1'"
 					sed -i 's/default/cluster1/g' /etc/rancher/k3s/k3s.yaml
 
+					# Add both configs to KUBECONFIG 
 					export KUBECONFIG=/etc/rancher/k3s/k3s.yaml:$MACHINE_TWO.config.yaml
+					mkdir -p .kube
+					echo "Building combine kube config file to connec to both clusters"
 					kubectl config view --flatten > ~/.kube/config
-					banner "Swithing kubectl context to cluster2"
+					echo "Swithing kubectl context to cluster2"
 					kubectl config use-context cluster2
+					echo "Avalable cluster context in kube config file"
 					kubectl config get-contexts
-					kubectl get nodes
+					echo "Getting Remote clusters [cluster2] nodes"
+					kubectl get nodes || { echo "********* Failed to get nodes from cluster [cluster2/$MACHINE_TWO] this needs tob e invastigated ********" && exit; }
 
-
+					banner "Logging into Remote machine to setup it's kubectl config file (this is optional step)"
 					ssh $MACHINE_TWO mkdir -p ~/.kube
 					ssh $MACHINE_TWO cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+					echo "===== WE ARE DONE ====="
 
 				else 
-				    echo "$FILE does not exist."
+				    echo "************ $FILE does not exist. - Failed to download it from $MACHINE_TWO ? ***************"
 				    exit
 				fi
 
         }
 elif [ "$HOST_NAME" = "$MACHINE_TWO" ]; then
         {
-                echo "Processing node01 installing k3s cluster"
+                banner "Processing $MACHINE_TWO installing k3s cluster - we will work on this cluster from machine [$MACHINE_ONE]"
                 curl -sfL https://get.k3s.io | sh -
         }
 else
 	{
-				echo "No Host matched for processing"
+				echo "No Host matched for processing - exiting the script without doing anything"
 	}
 fi
 
 
-
-
-#scp node01:/etc/rancher/k3s/k3s.yaml node01.config.yaml
