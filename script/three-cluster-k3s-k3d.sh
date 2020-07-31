9ffb9f56368350f4723d405b2c358ef1cdd9b0ee
Pranay Shah <pshah@tibco.com>
2020-07-31 13:12:03 -0700

    added script for creating cluster using k3d docker based clusters

--- script/three-cluster-k3s-k3d.sh
+++ script/three-cluster-k3s-k3d.sh
@@ -3,6 +3,8 @@
 ######################
 ### Helper script - 3 K8S cluster bsed on k3s in k3d (docker) - for kubectl "set-context" practice ####
 #https://www.katacoda.com/courses/kubernetes/launch-single-node-cluster
+
+#curl -sfL https://raw.githubusercontent.com/ishswar/two-clusters-on-katacoda/master/script/three-cluster-k3s-k3d.sh | bash -
 ######################
 
 set -o errexit
@@ -16,11 +18,17 @@ echo "============  $1  =============="
 echo "================================================"
 }
 
+spacer()
+{
+  printf "\n-----------------------------------------------------------------------------\n"
+}
 
 
-echo “Downloading k3d install script”
+
+banner “Downloading k3d install script”
 
 wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+spacer
 echo “Now creating k3s clusters in respective k3d docker container”
 banner "Creating cluster k8s"
 k3d cluster create k8s
@@ -29,6 +37,11 @@ k3d cluster create dk8s
 banner "Creating cluster nk8s"
 k3d cluster create nk8s
 #k3d cluster create sk8s
+
+spacer
+echo "List all the cluster contextes"
+
 kubectl config get-clusters
 
-echo “We are done”
+echo
+banner “We are done”
